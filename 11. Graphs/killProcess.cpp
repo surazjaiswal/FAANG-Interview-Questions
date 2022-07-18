b1ad901856c213acf8fcf5399aed1d44123ab41f
Suraj Jaiswal <surazjaiswal@gmail.com>
2022-07-18 23:42:14 +0530

    Graph Started

---
+++ 11. Graphs/killProcess.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/kill-process/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    return 0;
+}
